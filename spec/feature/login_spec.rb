1eecae92eadaea9e3b5297796831bd7b50b0fbf2
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-03-27 11:58:28 +0530

    add login feature specs

---
+++ spec/feature/login_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Login", :type => :feature do
+  let(:user) { create(:user) }
+
+  scenario 'user logs in and is redirected back to the root page', js: true do
+    visit root_path
+    expect(page).to have_current_path(new_user_session_path)
+
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'Log in'
+
+    expect(page).to have_current_path(root_path)
+    # expect(page).to have_content('You are logged in!')
+  end
+
+end
