ff573bb636adb60afd994650b5ccf18c35ef213c
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-03-27 16:41:26 +0530

    Add specs for User and Post models associations

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context 'Associations' do
+    it 'belongs_to user' do
+      association = described_class.reflect_on_association(:user).macro
+      expect(association).to eq :belongs_to
+    end
+  end
+end
