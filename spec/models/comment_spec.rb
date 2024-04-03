24c5eca8eacb04e36e1dd84303225e90d2d19dd3
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-04-03 18:58:55 +0530

    Add assosiation test for comment model

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Associations' do
+    it 'belongs_to user' do
+      association = described_class.reflect_on_association(:user).macro
+      expect(association).to eq :belongs_to
+    end
+  end
+  context 'Associations' do
+    it 'belongs_to post' do
+      association = described_class.reflect_on_association(:post).macro
+      expect(association).to eq :belongs_to
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,10 +7,17 @@ RSpec.describe Post, type: :model do
       expect(association).to eq :belongs_to
     end
   end
-  describe 'Attachment' do
+  describe 'Photo Attachment' do
     let(:post) { create(:post) }
     it 'is valid' do
       expect(post.photo).to be_attached
+    end
+  end
+  context 'Associations' do
+    it 'has_many comments' do
+      association = described_class.reflect_on_association(:comments)
+      expect(association.macro).to eq :has_many
+      expect(association.options[:dependent]).to eq :destroy
     end
   end
   
