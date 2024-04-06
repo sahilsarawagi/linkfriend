96bc5c16cb972e296ad438c05f5e987a8222e1fd
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-04-07 02:06:09 +0530

    Add follow and unfollow functionality - Added follow and unfollow buttons -Added follow and unfollow action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def follow
+    @user = User.find(params[:id])
+    current_user.followees << @user
+    redirect_back(fallback_location: users_path)
+  end
   
+  def unfollow
+    @user = User.find(params[:id])
+    current_user.followed_users.find_by(followee_id: @user.id).destroy
+    redirect_back(fallback_location: users_path)
+  end
+
 end
 
