b4831f72d193b782bcfcb39871fc8ac3dd36162f
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-04-09 20:01:36 +0530

    Implemented follow/unfollow button function in application_helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,17 @@ module ApplicationHelper
     end
   end
 
+  def follow_unfollow_button(user)
+    if user.id == current_user.id
+      {label: "Edit profile", path: edit_user_registration_path, method: :get}      
+    elsif current_user.followees.include?(user) 
+      {label: "Unfollow", path: unfollow_user_path(user), method: :post}
+    else 
+      {label: "Follow", path: follow_user_path(user), method: :post }
+    end
+                     
+  end
+
   def user_name(user)
     "#{user.first_name.capitalize} #{user.last_name.capitalize}"
   end
