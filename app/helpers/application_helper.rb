e82984d03865c1f202b5c3227934f4596eac5afd
sahil sarawagi <sahilsarawagi1@gmail.com>
2024-04-09 17:35:31 +0530

    Add profile photo to image tags.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def user_profile_photo(user)
-    if user.profile_photo.attached?
+    if user.profile_photo.attached? && user.profile_photo.persisted?
       user.profile_photo  
     else
       "/assets/default_profile.jpg"
